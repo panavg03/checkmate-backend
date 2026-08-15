d2d4482fc9893db4e65c42afcb82ad3130b9d07c
Kunsh <k789.tyagi@gmail.com>
2026-08-15 15:50:03 +0530

    pg code uncommented ertgryhtjhk

--- colyseus/src/rooms/GameRoom.ts
+++ colyseus/src/rooms/GameRoom.ts
@@ -71,14 +71,8 @@ export class GameRoom extends Room {
     onDrop(client: Client, code: number){
         //10 seconds for testing purposes
         // 4000 is Consented drop 
-        if(code !== 4000) this.allowReconnection(client, 30);
         //autosave state
         console.log(client.sessionId, " connection dropped");
-    }
-
-    onReconnect(client: Client){
-        //reconnection handling code
-        console.log(client.sessionId, " reconnected");
     }
 
     onLeave(client: Client, code:number){
