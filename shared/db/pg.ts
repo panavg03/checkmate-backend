f0da3be8da39ca71970591fb0c0de43762662cf0
Aargh Rai <shubh2007rai@gmail.com>
2026-08-15 16:13:53 +0530

    no ssl

--- shared/db/pg.ts
+++ shared/db/pg.ts
@@ -6,7 +6,7 @@ dotenv.config();
 /* Commented for demo
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: false,
 });*/
 const pool = new Pool({
     host: process.env.DATABASE_HOST,
@@ -14,7 +14,7 @@ const pool = new Pool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    ssl: { rejectUnauthorized: false },
+    ssl: false,
 });
 
 /** Asynchronously to verify -> PostgreSQL connection 
